1864c6fe0e0c5dda8e0330a70a9a19c0fa144fa6
RSDuck <Kemal-Afzal@outlook.de>
2017-08-02 02:15:35 +0200

    Added the ninja and shrinked down the tiles

--- source/game.nim
+++ source/game.nim
@@ -2,6 +2,8 @@ import nico
 
 proc gameInit() = 
     echo "init"
+    nico.loadPaletteFromGPL("palette.gpl")
+    nico.loadSpriteSheet("spritesheet.png")
 
 proc gameUpdate(dt: float) =
      discard
@@ -10,6 +12,9 @@ proc gameDraw() =
     cls()
     setColor(7)
     print("hello world", 42, 60)
+
+    nico.spr(38, 0, 0, 2, 2)
+    nico.spr(64, 0, 0, 2, 4)
 
 nico.init("impbox", "stranger_jam")
 
